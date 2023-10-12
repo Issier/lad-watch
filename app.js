472225bc7abdfb0e31f123627206504d3392f355
Ryan Collins <rjosephc3@gmail.com>
2023-10-12 18:10:56 -0400

    Added call to Riot API

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 import { API } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
+import axios from 'axios';
 
 const rest = new REST({ version: '10'}).setToken(process.env.DISCORD_TOKEN);
 
 const api = new API(rest);
 
-const guild = await api.guilds.getChannels('872984504770392094')
+const summInfo = (await axios.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/One%20Cappuccino', {
+    headers: {
+        "X-Riot-Token": process.env.RIOT_TOKEN
+    }
+})).data;
 
-console.log(guild)
+const name = summInfo.name;
+
+axios.get(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${summInfo.id}`, {
+    headers: {
+        "X-Riot-Token": process.env.RIOT_TOKEN
+    }
+}).then(response => {
+    console.log(`${name} is in a game`)
+}).catch(error => {
+    if (error.response && error.response.status === 404) {
+        console.log('Not in a game')
+    }
+})
