06443e470fb65464d46c4a5905e425c8fe005e43
Ryan Collins <rjosephc3@gmail.com>
2023-10-13 17:07:59 -0400

    Added Champion Mastery, Current Solo Queue Rank, and Live Games page

--- app.js
+++ app.js
@@ -38,22 +38,45 @@ axios.get(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summon
         }
     }
 
-    let gameTime = new Date(Date.now() - new Date(response.data.gameStartTime));
+    axios.get(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${summInfo.id}/by-champion/${summChar}`, {
+        headers: {
+            "X-Riot-Token": process.env.RIOT_TOKEN
+        }
+    }).then(async champMasteryResponse => {
+        let champMastery = champMasteryResponse.data.championPoints
 
-    const imageEmbed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle("LadWatch Alert")
-        .setDescription(bold(`${name} is playing ${champion} in ${gameType.description.replace(' games', '')}`))
-        .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/13.20.1/img/champion/${champion}.png`)
-        .setFields(
-            {name: '\u200B', value: '\u200B'},
-            {name: 'Game Time', value: `${gameTime.getMinutes()}:${gameTime.getSeconds().toString().padStart(2, '0')}`}
-        )
+        let gameTime = new Date(Date.now() - new Date(response.data.gameStartTime));
 
-    api.channels.createMessage(process.env.CHANNEL_ID, {
-        embeds: [imageEmbed]
-    });
-    console.log(`## ${name} is playing ${champion} in ${gameType.description}`)
+        let rankData = await axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summInfo.id}`, {
+            headers: {
+                "X-Riot-Token": process.env.RIOT_TOKEN
+            }
+        });
+
+        rankData = rankData.data.filter(data => data.queueType === 'RANKED_SOLO_5x5')[0]
+
+        const liveGamePages = `[u.gg](https://u.gg/lol/profile/na1/${encodeURIComponent(name)}/live-game) | ` +
+        `[op.gg](https://www.op.gg/summoners/na/${encodeURIComponent(name)}/ingame)`
+
+        const imageEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle("LadWatch Alert")
+            .setDescription(bold(`${name} is playing ${champion} in ${gameType.description.replace(' games', '')}`))
+            .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/13.20.1/img/champion/${champion}.png`)
+            .setFields(
+                {name: '\u200B', value: '\u200B'},
+                {name: 'Game Time', value: `${gameTime.getMinutes()}:${gameTime.getSeconds().toString().padStart(2, '0')}`, inline: true},
+                {name: 'Champion Mastery', value: `${champMastery.toLocaleString()}`, inline: true},
+                {name: 'Current Solo Queue Rank', value: !rankData ? 'Unranked' : `${rankData.tier} ${rankData.rank} ${rankData.leaguePoints}LP`, inline: true},
+                {name : 'Live Game Pages', value: liveGamePages}
+
+            )
+
+        api.channels.createMessage(process.env.CHANNEL_ID, {
+            embeds: [imageEmbed]
+        });
+        console.log(`## ${name} is playing ${champion} in ${gameType.description}`)
+    })
 }).catch(error => {
     if (error.response && error.response.status === 404) {
         console.log('Not in a game')
