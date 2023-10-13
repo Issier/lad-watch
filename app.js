55836f453e98c965d8d9c329fc054f7b53d7e53f
Ryan Collins <rjosephc3@gmail.com>
2023-10-13 16:29:50 -0400

    Added EmbedBuilder with info on GameTime and thumnail of champion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { API } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
-import { blockQuote } from '@discordjs/formatters';
+import { bold } from '@discordjs/formatters';
+import { EmbedBuilder } from '@discordjs/builders';
 import axios from 'axios';
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
@@ -37,8 +38,20 @@ axios.get(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summon
         }
     }
 
+    let gameTime = new Date(Date.now() - new Date(response.data.gameStartTime));
+
+    const imageEmbed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle("LadWatch Alert")
+        .setDescription(bold(`${name} is playing ${champion} in ${gameType.description.replace(' games', '')}`))
+        .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/13.20.1/img/champion/${champion}.png`)
+        .setFields(
+            {name: '\u200B', value: '\u200B'},
+            {name: 'Game Time', value: `${gameTime.getMinutes()}:${gameTime.getSeconds().toString().padStart(2, '0')}`}
+        )
+
     api.channels.createMessage(process.env.CHANNEL_ID, {
-        content: blockQuote(`## ${name} is playing ${champion} in ${gameType.description}`)
+        embeds: [imageEmbed]
     });
     console.log(`## ${name} is playing ${champion} in ${gameType.description}`)
 }).catch(error => {
