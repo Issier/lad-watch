de1d1c6499c9dd07befacde2d432b76357c62304
Ryan Collins <rjosephc3@gmail.com>
2023-12-02 12:38:46 -0500

    Fixed null reference error on summoner name

--- src/LoLAPI.js
+++ src/LoLAPI.js
@@ -32,49 +32,52 @@ export default async function fetchLeagueLadGameData(ladName, riotAPIToken) {
         /* Summoner Ranked Data */
         const rankData = (await axiosInstance.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summInfo.id}`)).data.filter(data => data.queueType === 'RANKED_SOLO_5x5')[0];
         /* Live Game Data */
-        let liveGame = (await axiosInstance.get(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${summInfo.id}`)).data;
-        const gameType = gameTypes.filter(val => val.queueId === liveGame.gameQueueConfigId)[0]
+        try {
+            let liveGame = (await axiosInstance.get(`https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${summInfo.id}`)).data;
+            const gameType = gameTypes.filter(val => val.queueId === liveGame.gameQueueConfigId)[0]
 
-        const summChar = liveGame.participants.filter(participant => {
-            return participant.summonerName === summInfo.name;
-        })[0].championId;
+            const summChar = liveGame.participants.filter(participant => {
+                return participant.summonerName === summInfo.name;
+            })[0].championId;
 
-        let champion = ""
-        for (const champ in champions.data) {
-            if (champions.data[champ].key == summChar) {
-                champion = champ;
+            let champion = ""
+            for (const champ in champions.data) {
+                if (champions.data[champ].key == summChar) {
+                    champion = champ;
+                }
+            }
+
+            const gameTime = new Date(Date.now() - new Date(liveGame.gameStartTime));
+
+            /* Live Game Champion Mastery for Summoner */
+            const champMastery = (await axiosInstance.get(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${summInfo.id}/by-champion/${summChar}`)).data.championPoints;
+
+            return {
+                gameTime: `${gameTime.getMinutes()}:${gameTime.getSeconds().toString().padStart(2, '0')}`,
+                champion: champion, 
+                championMastery: `${champMastery.toLocaleString()}`,
+                summonerId: summInfo.id,
+                summonerName: summInfo.name,
+                summonerRank: !rankData ? 'Unranked' : `${rankData.tier} ${rankData.rank} ${rankData.leaguePoints}LP`,
+                liveGamePages: `[u.gg](https://u.gg/lol/profile/na1/${encodeURIComponent(summInfo.name)}/live-game)` +  
+                            `| [op.gg](https://www.op.gg/summoners/na/${encodeURIComponent(summInfo.name)}/ingame)`,
+                gameType: gameType.description.replace(' games', ''),
+                rankColorHex: rankData.tier in rankColors ? rankColors[rankData.tier] : 0xFFFFFF,
+                gameId: liveGame.gameId,
+                hotStreak: rankData.hotStreak,
+                seasonWins: rankData.wins,
+                seasonLosses: rankData.losses
+            }
+        } catch (error) {
+            if (error.response.status === 404) {
+                logger.log({
+                    level: 'info',
+                    message: `Summoner ${summInfo.name} is not in a game`
+                })
             }
         }
-
-        const gameTime = new Date(Date.now() - new Date(liveGame.gameStartTime));
-
-        /* Live Game Champion Mastery for Summoner */
-        const champMastery = (await axiosInstance.get(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${summInfo.id}/by-champion/${summChar}`)).data.championPoints;
-
-        return {
-            gameTime: `${gameTime.getMinutes()}:${gameTime.getSeconds().toString().padStart(2, '0')}`,
-            champion: champion, 
-            championMastery: `${champMastery.toLocaleString()}`,
-            summonerId: summInfo.id,
-            summonerName: summInfo.name,
-            summonerRank: !rankData ? 'Unranked' : `${rankData.tier} ${rankData.rank} ${rankData.leaguePoints}LP`,
-            liveGamePages: `[u.gg](https://u.gg/lol/profile/na1/${encodeURIComponent(summInfo.name)}/live-game)` +  
-                           `| [op.gg](https://www.op.gg/summoners/na/${encodeURIComponent(summInfo.name)}/ingame)`,
-            gameType: gameType.description.replace(' games', ''),
-            rankColorHex: rankData.tier in rankColors ? rankColors[rankData.tier] : 0xFFFFFF,
-            gameId: liveGame.gameId,
-            hotStreak: rankData.hotStreak,
-            seasonWins: rankData.wins,
-            seasonLosses: rankData.losses
-        }
-        
     } catch (error) {
-        if (error.response.status === 404) {
-            logger.log({
-                level: 'info',
-                message: `Summoner ${summInfo.name} is not in a game`
-            })
-        } else if (error.response.status < 500) {
+        if (error.response.status < 500) {
             logger.log({
                 level: 'error',
                 message: 'Failed to fetch league lad data'
